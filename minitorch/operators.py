b72d640d3055fee39ffd56b844ca53a4f8a50d9c
Бугаев Егор <epbugaev@Bugaevs-MacBook-Pro.local>
2024-11-04 11:37:07 +0300

    Fix style

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -36,54 +36,68 @@ from typing import Callable, Iterable
 def mul(a, b):
     return a * b
 
+
 def id(a):
     return a
+
 
 def add(a, b):
     return a + b
 
+
 def neg(a):
     return -a
+
 
 def lt(a, b):
     return a < b
 
+
 def eq(a, b):
     return a == b
+
 
 def max(a, b):
     return a if a > b else b
 
+
 def is_close(a, b):
     return abs(a - b) < 1e-2
 
+
 def sigmoid(a):
-    #if a < 0:
     return 1 / (1 + math.exp(-1 * a))
-   #return math.exp(a) / (1 + math.exp(a))
+
 
 def relu(a):
     return max(0, a)
 
+
 def log(a): 
     return math.log(a)
+
 
 def exp(a):
     return math.exp(a)
 
+
 def inv(a):
     return 1 / a
+
 
 def log_back(a, b):
     return b / a
 
+
 def inv_back(a, b):
     return - b / (a * a)
+
 
 def relu_back(a, b):
     if a < 0:
         return 0
     return b
+
 
 # ## Task 0.3
 
@@ -106,9 +120,11 @@ def map(f: Callable, a: Iterable):
     for item in a: 
         yield f(item)
 
+
 def zipWith(f: Callable, a: Iterable, b: Iterable):
     for item_a, item_b in zip(a, b):
         yield f(item_a, item_b)
+
 
 def reduce(f: Callable, a: Iterable):
     res = None
@@ -119,17 +135,21 @@ def reduce(f: Callable, a: Iterable):
             res = f(res, item)
     return res
 
+
 def negList(a: list):
     return list(map(neg, a))
 
+
 def addLists(a: Iterable, b: Iterable):
     return zipWith(add, a, b)
+
 
 def sum(a: Iterable):
     res = reduce(add, a)
     if res is None:
         return 0
     return res
+
 
 def prod(a: Iterable):
     return reduce(mul, a)
